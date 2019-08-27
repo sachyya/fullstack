529a27a07f114d99ecb21a7a656dc1ff983a81d5
sachyya <sachyya1@gmail.com>
2019-08-27 17:36:00 +0000

    Knew about state hooks

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,15 +1,50 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const App = (props) => {
-	const [counter, setCounter ] = useState(0)
+const History = (props) => {
+	if ( props.allClicks.length === 0 ) {
+		return(
+			<div>the app is used by pressing the buttons</div>
+		)
+	}
 
-	const setCounterValue = (value) => () => setCounter( value )
+	return(
+		<div>button press history: {props.allClicks.join(' ')}</div>
+	)
+}
+
+const Button = (props) => {
+	console.log('props value is', props)
+	const { onClick, text } = props
+	return(  
+		<button onClick={onClick}>    {text}  </button>
+	)
+
+}
+
+const App = (props) => {
+	const [left, setLeft] = useState(0)
+	const [right, setRight] = useState(0)
+	const [allClicks, setAll] = useState([])
+
+	const handleLeftClick = () => {
+		setAll(allClicks.concat('L'))    
+		setLeft(left + 1)
+	}
+
+	const handleRightClick = () => {
+	    setAll(allClicks.concat('R'))    
+	    setRight(right + 1)
+	}
 	return (
 		<div>
-			<div>{counter}</div>
-			<button onClick={setCounterValue( counter + 1 )}>plus</button>
-			<button onClick={setCounterValue(0)}>reset</button>
+			{left}
+			<Button onClick={handleLeftClick} text='left' />
+			<Button onClick={handleRightClick} text='right' />
+			{right}
+
+			<History allClicks={allClicks} />
+
 		</div>
 	)
 }
