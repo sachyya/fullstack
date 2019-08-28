eba4018d725bb2bb2802ea0e91e1e3ab9d5f969f
sachyya <sachyya1@gmail.com>
2019-08-28 13:32:54 +0000

    anecdotes step1

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,89 +1,32 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Heading = (props) => <h2>{props.title}</h2>
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
 
-const Button = (props) => {
-	return(
-		<button onClick={props.onClick}>{props.text}</button>
-	)
+  const onClick = () => {
+  	let ran_num = Math.floor((Math.random() * 6) + 0)
+	setSelected(0 + ran_num)
+  }
+
+  return (
+    <div>
+      <p>{props.anecdotes[selected]}</p>
+      <button onClick={onClick}>next anecdote!</button>
+    </div>
+  )
 }
 
-const Statistic = ({text, value}) => {
-	return(
-		<tr>
-			<th>{text}</th>
-			<th>{value}</th>
-		</tr>
-	)
-}
+const anecdotes = [
+	'If it hurts, do it more often',
+	'Adding manpower to a late software project makes it later!',
+	'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+	'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+	'Premature optimization is the root of all evil.',
+	'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
-const Statistics = ({statistics}) => {
-
-	if (statistics.total <= 0 ) {
-		return <p>No feedbacks given</p>
-	}
-	return(
-		<table>
-			<tbody>
-				<Statistic text="good" value={statistics.good}	/>
-				<Statistic text="bad" value={statistics.bad}	/>
-				<Statistic text="neutral" value={statistics.neutral}	/>
-				<Statistic text="total" value={statistics.total}	/>
-				<Statistic text="average" value={statistics.average}	/>
-				<Statistic text="positive" value={statistics.precentage}	/>
-			</tbody>
-		</table>
-	)
-}
-
-const App = () => {
-	// save clicks of each button to own state
-	const [good, setGood] = useState(0)
-	const [neutral, setNeutral] = useState(0)
-	const [bad, setBad] = useState(0)
-
-	const goodClick = () => {
-		setGood( good + 1 )
-	}
-
-	const neutralClick = () => {
-		setNeutral( neutral + 1 )
-	}
-
-	const badClick = () => {
-		setBad( bad + 1 )
-	}
-
-	const total = () => good + bad + neutral
-
-	const precentage = () => ( good/total() ) * 100 + ' %'
-	const average = () => ( total()/3 )
-
-	let statistics = 
-	{
-		bad: bad,
-		good: good,
-		neutral: neutral,
-		total: total(),
-		average: average(),
-		precentage: precentage()
-	}
-
-	// console.log(statistics)
-	return (
-		<div>
-			<Heading title="give feedback"/>			
-			<Button onClick={goodClick} text="good" />			
-			<Button onClick={neutralClick} text="neutral" />			
-			<Button onClick={badClick} text="bad" />			
-			
-			<Heading title="statistics"/>	
-			<Statistics statistics={statistics} />
-		</div>
-	)
-}
-
-ReactDOM.render(<App />, 
-	document.getElementById('root')
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
 )
