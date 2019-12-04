bb6a78a4edc89ae0f440c65019bc5a08d1fec03e
sachyya <sachyya1@gmail.com>
2019-12-04 17:13:06 +0000

    unicafe step2

--- part1revised/unicafe/src/index.js
+++ part1revised/unicafe/src/index.js
@@ -13,17 +13,41 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allClicks, setClicks] = useState(0)
+
+  const handleGoodClick = () => {
+  	setGood(good + 1)
+  	setClicks(allClicks + 1)
+  }
+
+  const handleNeutralClick = () => {
+  	setNeutral(neutral + 1)
+  	setClicks(allClicks + 1)
+  }
+
+  const handleBadClick = () => {
+  	setBad(bad + 1)
+  	setClicks(allClicks + 1)
+  }
+
+  const positiveFeedback = () => good/allClicks * 100 + '%';
+
+  const avgFeedback = () => allClicks/3;
+
 
   return (
     <div>
     	<Heading text="give feedback" /> 
-	    	<Button onClick={() => setGood(good + 1)} text="good" />
-	    	<Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
-	    	<Button onClick={() => setBad(bad + 1)} text="bad" />
+	    	<Button onClick={handleGoodClick} text="good" />
+	    	<Button onClick={handleNeutralClick} text="neutral" />
+	    	<Button onClick={handleBadClick} text="bad" />
     	<Heading text="statistics" /> 
 	    	<Result text="good" count={good} /> 
 	    	<Result text="neutral" count={neutral}/> 
 	    	<Result text="bad" count={bad}/> 
+	    	<Result text="all" count={allClicks}/> 
+	    	<Result text="average" count={avgFeedback()}/> 
+	    	<Result text="positive" count={positiveFeedback()}/> 
     </div>
   )
 }
