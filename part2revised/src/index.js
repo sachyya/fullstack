b5354be0131f8ed1fe8b0be1ddc644e335c108d0
sachyya <sachyya1@gmail.com>
2019-12-08 15:59:30 +0000

    Finished exercise in par2 starting

--- part2revised/src/index.js
+++ part2revised/src/index.js
@@ -1,62 +1,29 @@
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-const History = (props) => {
-	if (props.allClicks.length === 0 ) {
-		return(
-			<div>
-				the app is used by pressing buttons
-			</div>
-		)
-	} 
-
-	return (
-		<div>
-			button press history: {props.allClicks.join(' ')}
-		</div>
-	)
-}
-
-
-const Display = (props) => <div>{props.value}</div>
-
-const Button = ({onClick, text}) => {
-	return (
-		<button onClick={onClick} >
-			{text}
-		</button>
-	)
-}
-
-const App = (props) => {
-  const [clicks, setClicks] = useState({left: 0, right: 0})
-  const [allClicks, setAll] = useState([])
-
-  const handleLeftClick = () => {
-  	setClicks({...clicks, left: clicks.left + 1})
-  	setAll(allClicks.concat('L'))
+const notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only Javascript',
+    date: '2019-05-30T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true
   }
-
-  const handleRightClick = () => {
-  	setClicks( {...clicks, right: clicks.right + 1} )
-  	setAll(allClicks.concat('R'))
-  }
-  console.log('props is',props)
-  return (
-    <div>
-      <div>
-        <Display value={clicks.left} />
-        <Button onClick={handleLeftClick} text="left" />
-        <Button onClick={handleRightClick} text="right" />
-        <Display value={clicks.right} />
-        <History allClicks={allClicks} />
-      </div>
-    </div>
-  )
-}
-
+]
 
 ReactDOM.render(
-	<App />,
-	document.getElementById('root')
+  <App notes={notes} />,
+  document.getElementById('root')
 )
